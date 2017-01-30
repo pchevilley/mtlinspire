a9459c9a86a127c93fde8285abcda518af8182a2
Paul Chevilley <chevpa@2020spaces.com>
2017-01-30 10:19:11 -0500

    add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import "rxjs/Rx";
+
+declare var ga: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+      constructor(public router: Router) {
+          router.events.distinctUntilChanged((previous: any, current: any) => {
+              if(current instanceof NavigationEnd) {
+                  return previous.url === current.url;
+              }
+              return true;
+          }).subscribe((x: any) => {
+              ga('send', 'pageview', x.url);
+          });
+      }
+}
